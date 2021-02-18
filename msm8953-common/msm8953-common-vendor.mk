b1cc26ad990ebe57ca7ffffc48d4db534bea2db1
TogoFire <italomellopereira@gmail.com>
2021-02-18 14:27:42 -0300

    msm8953-common: Drop libmmparserextractor if use wfd legacy
    When using wfd legacy, if you do not remove this lib libmmparserextractor you will have problems such as: music player cannot play the songs, sd card is not detected by some apps, on cameras the photos and videos will be slow to respond and gcam force close.
    
    -log:
    02-17 16:09:59.817  8853  8853 F DEBUG   : Abort message: 'Check failed: libHandle != nullptr couldn't dlopen(/system_ext/lib64/extractors/libmmparserextractor.so) No such file or directory'
    
    https://github.com/Dev-msm8953/frameworks_av/commit/8364158603cc973a388d37350c966587faa0b9a6
    
    https://github.com/Dev-msm8953/frameworks_av/commit/46ae13e87892f6332c159dd99c3da2fd450a85e6
    
    https://gerrit.syberiaos.com/q/topic:%22legacy-wfd-a11%22+(status:open%20OR%20status:merged)
    
    Change-Id: 270552327ad3964f11907e47e88ff9df2765c592
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- msm8953-common/msm8953-common-vendor.mk
+++ msm8953-common/msm8953-common-vendor.mk
@@ -161,8 +161,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/msm8953-common/proprietary/product/lib64/vendor.qti.imsrtpservice@2.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/vendor.qti.imsrtpservice@2.0.so \
     vendor/xiaomi/msm8953-common/proprietary/product/lib64/vendor.qti.imsrtpservice@2.1.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/vendor.qti.imsrtpservice@2.1.so \
     vendor/xiaomi/msm8953-common/proprietary/product/lib64/vendor.qti.latency@2.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/vendor.qti.latency@2.0.so \
-  	vendor/xiaomi/msm8953-common/proprietary/system_ext/lib/extractors/libmmparserextractor.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/extractors/libmmparserextractor.so \
-    vendor/xiaomi/msm8953-common/proprietary/system_ext/lib64/extractors/libmmparserextractor.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/extractors/libmmparserextractor.so \
     vendor/xiaomi/msm8953-common/proprietary/vendor/bin/ATFWD-daemon:$(TARGET_COPY_OUT_VENDOR)/bin/ATFWD-daemon \
     vendor/xiaomi/msm8953-common/proprietary/vendor/bin/SoterProvisioningTool:$(TARGET_COPY_OUT_VENDOR)/bin/SoterProvisioningTool \
     vendor/xiaomi/msm8953-common/proprietary/vendor/bin/adsprpcd:$(TARGET_COPY_OUT_VENDOR)/bin/adsprpcd \
